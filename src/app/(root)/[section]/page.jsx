07587ed2824e3205a2f49ccee3531e37c2258dc3
Marvelous Iretomiwa <Marveltoms2014@gmail.com>
2026-01-15 17:55:45 +0100

    display pages

--- src/app/(root)/[section]/page.jsx
+++ src/app/(root)/[section]/page.jsx
@@ -2,8 +2,8 @@ import dynamic from 'next/dynamic'
 import { notFound } from 'next/navigation'
 
 const sections = {
+    PersonalProfile: dynamic(() => import('./sections/PersonalProfile')),
   Notification: dynamic(() => import('./sections/Notification')),
-  PersonalProfile: dynamic(() => import('./sections/PersonalProfile')),
   community: dynamic(() => import('./sections/Community')),
   frames: dynamic(() => import('./sections/Frames')),
   dates: dynamic(() => import('./sections/Dates')),
@@ -12,13 +12,14 @@ const sections = {
   settings: dynamic(() => import('./sections/Settings')),
 }
 
-export default function SectionPage({ params }) {
-  const { section } = params
+export default async function SectionPage({ params }) {
+  const { section } = await params   
+
   const SectionComponent = sections[section]
-  
+
   if (!SectionComponent) {
     notFound()
   }
-  
+
   return <SectionComponent />
 }
